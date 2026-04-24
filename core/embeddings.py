6e6a47077b5a822ec35b06cfd075576cfb73223b
Sreeharish Rathakrishnan <sreeharish.r@xencia.com>
2026-04-24 17:12:26 +0530

    updated azure

--- core/embeddings.py
+++ core/embeddings.py
@@ -1,4 +1,10 @@
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai import AzureOpenAIEmbeddings
+import os
 
 def get_embeddings():
-    return OpenAIEmbeddings()
+    return AzureOpenAIEmbeddings(
+        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+        deployment=os.getenv("AZURE_OPENAI_EMBEDDING_DEPLOYMENT"),
+        api_version="2024-02-15-preview"
+    )
--- core/qa_chain.py
+++ core/qa_chain.py
@@ -1,9 +1,13 @@
+from langchain_openai import AzureChatOpenAI
 from langchain.chains import RetrievalQA
-from langchain_openai import ChatOpenAI
+import os
 
 def build_qa_chain(retriever):
-    llm = ChatOpenAI(
-        model="gpt-4o-mini",
+    llm = AzureChatOpenAI(
+        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+        deployment_name=os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME"),
+        api_version="2024-02-15-preview",
         temperature=0
     )
 
