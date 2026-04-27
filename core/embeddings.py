0e0a9ce2dfde92d628c72922cc1ada820415dba1
Sreeharish Rathakrishnan <sreeharish.r@xencia.com>
2026-04-27 12:00:55 +0530

    allminiLml6v2

--- core/embeddings.py
+++ core/embeddings.py
@@ -1,10 +1,6 @@
-from langchain_openai import AzureOpenAIEmbeddings
-import os
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 def get_embeddings():
-    return AzureOpenAIEmbeddings(
-        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-        deployment=os.getenv("AZURE_OPENAI_EMBEDDING_DEPLOYMENT"),
-        api_version="2024-02-15-preview"
+    return HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
